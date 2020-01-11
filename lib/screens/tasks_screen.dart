20c8c47abd864cef42e04ff2d679138587d1d44b
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-12 01:58:44 +0900

    extract ListView and ListTile

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -58,25 +58,41 @@ class TasksScreen extends StatelessWidget {
                     topRight: Radius.circular(20.0),
                   ),
                 ),
-                child: ListView(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text('This is a task.'),
-                      trailing: Checkbox(value: false, onChanged: null),
-                    ),
-                    ListTile(
-                      title: Text('This is a task.'),
-                      trailing: Checkbox(value: false, onChanged: null),
-                    ),
-                    ListTile(
-                      title: Text('This is a task.'),
-                      trailing: Checkbox(value: false, onChanged: null),
-                    ),
-                  ],
-                )),
+                child: TasksList()),
           )
         ],
       ),
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+  const TasksList({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('This is a task.'),
+      trailing: Checkbox(value: false, onChanged: null),
     );
   }
 }
