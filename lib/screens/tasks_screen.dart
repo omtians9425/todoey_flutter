db15a538c7d452b17d6eac734240182886eaf930
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-12 01:15:00 +0900

    modify icon

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,24 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding:
+            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+              radius: 30.0,
+              backgroundColor: Colors.white,
+            ),
+            Text('Todoey'),
+          ],
+        ),
+      ),
     );
   }
 }
