63adb15b3e51f6319b124e48efe73b8d7d290a9e
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-12 23:59:32 +0900

    extract widget

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 import 'package:todoey_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -11,7 +12,7 @@ class TasksScreen extends StatelessWidget {
         child: Icon(Icons.add),
         onPressed: () {
           showModalBottomSheet(
-              context: context, builder: (context) => Container());
+              context: context, builder: (context) => AddTaskScreen());
         },
       ),
       body: Column(
