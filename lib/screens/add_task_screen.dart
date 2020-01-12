15188d4aefa4ada440cfad7f8692223b68e77a81
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-13 00:22:19 +0900

    implement bottom sheet's layout

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,12 +6,45 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(30.0),
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            )),
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            TextFormField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton(
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
