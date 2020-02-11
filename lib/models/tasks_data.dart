b90c34c5507df608ad96cf82ccfe68ddea7aa549
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2020-02-11 13:21:12 +0900

    make tasks private and add getter

--- lib/models/tasks_data.dart
+++ lib/models/tasks_data.dart
@@ -2,19 +2,23 @@ import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/models/task.dart';
 
 class TasksData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'buy milk'),
     Task(name: 'buy cake'),
     Task(name: 'go gym')
   ];
 
+  List<Task> get tasks {
+    return _tasks;
+  }
+
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 }
